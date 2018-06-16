6bd6d49a2b3fd6898c2fc84fea3f580746442621
mpudzeis <40313883+mpudzeis@users.noreply.github.com>
2018-06-16 11:33:49 +0200

    Update AutoDM.js

--- src/AutoDM.js
+++ src/AutoDM.js
@@ -41,7 +41,7 @@ const GenerateMessage = name => {
   ];
   const d = new Date();
   const dayName = days[d.getDay()];
-  return `Hi ${name} Thanks for .... \n Happy ${dayName} 😊😊 `; // your message
+  return `Hi ${name} Thanks for following us. Our goal is to end Cyberbullying by preventing it through education. \n Happy to discuss anytime 😊  \n Happy ${dayName} 😊😊 \n Pssst: Don’t hesitate to visit our webpage and support our cause 👉 https://goo.gl/1F8KpK 😊`; // your message
   // My message   return `Hi ${name} Thanks for being a part of my social media network. I'am the @PicsrushE founder,A new Online Image Editor completely with web technologies,I'm also a reactjs developer and medium blogger.\n Happy to discuss anytime 😊  \n Happy ${dayName} 😊😊 `;
 };
 
